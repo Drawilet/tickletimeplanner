8e92b48d797c7de57d94eb0163bdfa9ad93915c3
Drawilet <drawiletdev@gmail.com>
2023-11-08 14:00:20 -0600

    project base

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,12 +26,10 @@
 <body class="font-sans antialiased">
     <x-banner />
 
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @livewire('navigation-menu')
-
+    <div class="min-h-screen">
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
+            <header class="bg-base-200 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
