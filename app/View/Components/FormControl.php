5087d936c9f4c391f8fd5765aef5857d5d8d5f25
Drawilet <drawiletdev@gmail.com>
2023-11-08 21:04:37 -0600

    refactor: change form-control to FormControl

--- app/View/Components/FormControl.php
+++ app/View/Components/FormControl.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class form-control extends Component
+class FormControl extends Component
 {
     /**
      * Create a new component instance.
