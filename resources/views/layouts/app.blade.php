8ad911c46e40a38d3d68de5cebe0872c2ede38b9
Drawilet <drawiletdev@gmail.com>
2023-11-08 20:59:33 -0600

    feat: Add NavbarComponent and update Dropdown and Sidebar components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,25 +27,10 @@
     <x-banner />
 
     <div class="min-h-screen">
+        <x-navbar-component />
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-base-200 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-
-        <!-- Page Content -->
-        <main class="flex-1">
-            <div class="flex h-screen">
-                <div class="sidebar h-full w-[10px]  bg-gray-100 dark:bg-gray-900">
-                    <x-sidebar-component></x-sidebar-component>
-                <div class="flex-1">
-                    {{ $slot }}
-                </div>
-            </div>
+        <main class="">
+            {{ $slot }}
         </main>
     </div>
 
