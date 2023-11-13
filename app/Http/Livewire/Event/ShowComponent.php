53e6a013a547145893a1856c36b18f0252b902f1
Drawilet <drawiletdev@gmail.com>
2023-11-13 13:50:07 -0600

    feat: add event show component

---
+++ app/Http/Livewire/Event/ShowComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Event;
+
+use Livewire\Component;
+
+class ShowComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.event.show-component');
+    }
+}
