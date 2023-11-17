8ac17cb976f10cd2b26283dfc34d9a0985a121de
Sramos2077 <2110303@utmatamoros.edu.mx>
2023-11-17 09:30:11 -0600

    Feat: delete some design text

--- resources/views/components/sidebar-component.blade.php
+++ resources/views/components/sidebar-component.blade.php
@@ -1,5 +1,5 @@
-<div class="drawer max-w-[250px]">
-    <input id="sidebar" type="checkbox" class="drawer-toggle" />
+<div class="drawer">
+    <input id="sidebar" type="checkbox" class="drawer-toggle"/>
     <div class="drawer-content">
         <label for="sidebar" class="btn btn-square btn-ghost">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -15,23 +15,18 @@
         <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-truncate">
             <a class="btn btn-ghost normal-case text-xl">Menu</a>
             <!-- Sidebar content here -->
-            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="route()">
+            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                 </svg>
                 <span>customers</span>
             </a>
-            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="{{ route('products') }}">
+            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                 </svg>
                 <span>products</span>
             </a>
-
-                {{-- <div class="w-full flex items-center gap-x-1.5 group select-none">
-
-                    <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm" href="route()">products</a>
-                </div> --}}
         </ul>
     </div>
 </div>
