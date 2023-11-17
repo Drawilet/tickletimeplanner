af13b20f07cda2584061f2bbcb0476c62c017709
Sramos2077 <2110303@utmatamoros.edu.mx>
2023-11-17 09:29:16 -0600

    Feat: sidebar design

--- resources/views/components/sidebar-component.blade.php
+++ resources/views/components/sidebar-component.blade.php
@@ -1,4 +1,4 @@
-<div class="drawer">
+<div class="drawer max-w-[250px]">
     <input id="sidebar" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
         <label for="sidebar" class="btn btn-square btn-ghost">
@@ -12,22 +12,26 @@
     <div class="drawer-side">
         <label for="sidebar" aria-label="close sidebar" class="drawer-overlay">
         </label>
-        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-truncate">
             <a class="btn btn-ghost normal-case text-xl">Menu</a>
             <!-- Sidebar content here -->
-                <div class="w-full flex items-center gap-x-1.5 group select-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                        <style>svg{fill:#686c2d}</style><path d="M112 48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm40 304V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V256.9L59.4 304.5c-9.1 15.1-28.8 20-43.9 10.9s-20-28.8-10.9-43.9l58.3-97c17.4-28.9 48.6-46.6 82.3-46.6h29.7c33.7 0 64.9 17.7 82.3 46.6l58.3 97c9.1 15.1 4.2 34.8-10.9 43.9s-34.8 4.2-43.9-10.9L232 256.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V352H152z"/>
-                    </svg>
-                    <x-nav-link href="route()">Customers</x-nav-link>
-            </div>
-
-            <div class="w-full flex items-center gap-x-1.5 group select-none">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                    <style>svg{fill:#737017}</style><path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
+            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="route()">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                 </svg>
-                <x-nav-link href="route()">Products</x-nav-link>
-            </div>
+                <span>customers</span>
+            </a>
+            <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm flex justify-start items-center" href="{{ route('products') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+                <span>products</span>
+            </a>
+
+                {{-- <div class="w-full flex items-center gap-x-1.5 group select-none">
+
+                    <a class="btn btn-xs sm:btn-sm md:btn-md lg:btn-sm" href="route()">products</a>
+                </div> --}}
         </ul>
     </div>
 </div>
