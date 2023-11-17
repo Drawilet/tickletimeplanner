3c1cef67eb0f91e733d3ed729f162fa31a63c000
Drawilet <drawiletdev@gmail.com>
2023-11-17 12:03:56 -0600

    feat: Add display mode and search designs to event show component

--- resources/views/livewire/Event/show-component.blade.php
+++ resources/views/livewire/Event/show-component.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- Be like water. --}}
+<div class="">
+    <div class="max-w-xs p-2 rounded shadow-lg">
+        <div class="tabs tabs-lifted">
+            @foreach (['table', 'calendar'] as $mode)
+                <a class="capitalize tab {{ $mode == $filter['display_mode'] ? 'tab-active' : '' }}"
+                    wire:click='changeDisplayMode("{{ $mode }}")'>{{ $mode }}</a>
+            @endforeach
+        </div>
+
+        <x-input id="search" name="search" wire:model="filter.search" placeholder="Search event" />
+
+        <span class="text-sm ml-2 mt-2 block">4 events found</span>
+
+    </div>
 </div>
