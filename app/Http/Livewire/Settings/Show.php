c2da46a21b2308260ebdedbe5e5f7462f3a3ac42
Drawilet <drawiletdev@gmail.com>
2023-11-17 10:15:21 -0600

    feat: Add settings functionality to the application

--- app/Http/Livewire/Settings/Show.php
+++ app/Http/Livewire/Settings/Show.php
@@ -2,14 +2,60 @@
 
 namespace App\Http\Livewire\Settings;
 
+use App\Models\Setting;
 use Livewire\Component;
 
 class Show extends Component
 {
-    
+    public $initialData = [
+        "name" => "",
+        "schedule" => [],
+        "opening" => "",
+        "closing" => "",
+    ];
+    public $data;
+
+    public $days = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ];
+    public function mount()
+    {
+        $this->data = Setting::first()?->toArray() ?? $this->initialData;
+    }
 
     public function render()
     {
         return view('livewire.settings.show');
+    }
+
+    public function toggleDay($day)
+    {
+        if (in_array($day, $this->data['schedule'])) {
+            $this->data['schedule'] = array_diff($this->data['schedule'], [$day]);
+        } else {
+            array_push($this->data['schedule'], $day);
+        }
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'data.name' => 'required',
+            'data.schedule' => 'required',
+            'data.opening' => 'required',
+            'data.closing' => ['required', 'after:data.opening'],
+        ]);
+
+        Setting::updateOrCreate(
+            ['id' => 1],
+            $this->data
+        );
+        $this->emit('saved');
     }
 }
--- resources/views/livewire/settings/show.blade.php
+++ resources/views/livewire/settings/show.blade.php
@@ -3,7 +3,8 @@
         <h2 class="text-xl">General</h2>
         <x-form-control>
             <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" name="name" wire:model="name" />
+            <x-input id="name" name="name" wire:model="data.name" />
+            <x-input-error for="data.name" class="mt-2" />
         </x-form-control>
     </section>
 
@@ -11,25 +12,27 @@
         <h2 class="text-xl">Schedule</h2>
 
         <div class="join mt-3">
-            <button class="join-item btn btn-neutral">S</button>
-            <button class="join-item btn btn-neutral">M</button>
-            <button class="join-item btn btn-neutral">T</button>
-            <button class="join-item btn btn-neutral">W</button>
-            <button class="join-item btn btn-neutral">T</button>
-            <button class="join-item btn btn-active">F</button>
-            <button class="join-item btn btn-active">S</button>
+            @foreach ($days as $day)
+                <button wire:click='toggleDay("{{ $day }}")'
+                    class="join-item btn {{ in_array($day, $data['schedule']) ? 'btn-neutral' : 'btn-active' }}">{{ substr($day, 0, 1) }}</button>
+            @endforeach
         </div>
 
         <x-form-control>
             <x-label for="opening" value="{{ __('Opening') }}" />
-            <x-input id="opening" name="opening" wire:model="opening" type="time" />
+            <x-input id="opening" name="opening" wire:model="data.opening" type="time" />
+            <x-input-error for="data.opening" class="mt-2" />
         </x-form-control>
 
         <x-form-control>
             <x-label for="closing" value="{{ __('Closing') }}" />
-            <x-input id="closing" name="closing" wire:model="closing" type="time" />
+            <x-input id="closing" name="closing" wire:model="data.closing" type="time" />
+            <x-input-error for="data.closing" class="mt-2" />
         </x-form-control>
     </section>
 
-    <button class="btn btn-primary w-full">Save</button>
+    <button class="btn btn-primary w-full" wire:click="save">Save</button>
+    <x-action-message class="mt-3" on="saved">
+        {{ __('Saved.') }}
+    </x-action-message>
 </div>
