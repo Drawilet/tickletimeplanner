96af49dabf2cbb98a023eb19c2972e0a1f8328b6
Alonzo997 <alonzojesuseduardo@gmail.com>
2023-11-17 13:42:57 -0600

    fead:Add Customer model and migration, update authentication card and button components, and modify web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Clientcomponent;
+use App\Http\Livewire\Customercomponent;
+use App\Http\Livewire\CustomerModelscomponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');});
+    
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+Route::get('/CustomerModelscomponent', Customercomponent::class)->name('Clientes');
+
+
+
+
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
