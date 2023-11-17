8bac19bdfcaeb42923d2210e2b3b00a895a27367
Drawilet <drawiletdev@gmail.com>
2023-11-17 13:48:24 -0600

    feat: add calendar and table component in show events component (only designs)

---
+++ app/Http/Livewire/Event/ShowComponent/TableComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Event\ShowComponent;
+
+use Livewire\Component;
+
+class TableComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.event.show-component.table-component');
+    }
+}
---
+++ resources/views/livewire/Event/show-component/table-component.blade.php
@@ -0,0 +1,33 @@
+<div class="overflow-x-auto">
+    <table class="table table-zebra">
+        <!-- head -->
+        <thead>
+            <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Customer</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Start time</th>
+                <th>End time</th>
+                <th>Price</th>
+                <th>Notes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- row 1 -->
+            <tr>
+                <td>1</td>
+                <td>Example Event</td>
+                <td>John Doe</td>
+                <td>Example Location</td>
+                <td>2022-01-01</td>
+                <td>10:00 AM</td>
+                <td>12:00 PM</td>
+                <td>$100</td>
+                <td>Example Notes</td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
