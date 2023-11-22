b3e793c445de247907fee18d5f3096f1527f8fa0
Drawilet <drawiletdev@gmail.com>
2023-11-22 11:13:22 -0600

    feat: customer component uses crud component

--- app/Http/Livewire/Customer/ShowComponent.php
+++ app/Http/Livewire/Customer/ShowComponent.php
@@ -2,74 +2,24 @@
 
 namespace App\Http\Livewire\Customer;
 
+use App\Events\CustomerEvent;
+use App\Http\Livewire\Util\CrudComponent;
 use Livewire\Component;
 use App\Models\Customer;
 use Livewire\WithFileUploads;
 use Livewire\WithPagination;
 
-class ShowComponent extends Component
+class ShowComponent extends CrudComponent
 {
-    use WithFileUploads,  WithPagination;
-    public $set;
-    public $photo, $buisinessname, $tradename;
-    public $id_Customer;
-    public $search;
-    public $modal;
-    public $user;
-    public $photoPath;
+    protected $listeners =  ["socket" => "socketHandler"];
 
-    public function render()
-{
-    return view('livewire.customer.show-component', [
-        'Customer' =>Customer::paginate(1),
-    ]);
-}
-
-    public function OpenModal()
-    {
-        $this->modal = true;
-    }
-
-    public function Offmodal()
-    {
-        $this->modal = false;
-    }
-
-    public function cleanfields()
-    {
-        $this->photo = '';
-        $this->buisinessname = '';
-        $this->tradename = '';
-    }
-    public function edit($id)
-    {
-        $Customer = Customer::findOrFail($id);
-        $this->id_Customer = $id;
-        $this->photo = $Customer->photo;
-        $this->buisinessname = $Customer->buisinessname;
-        $this->tradename = $Customer->tradename;
-        $this->OpenModal();
-    }
-
-    public function delete($id)
-    {
-        $this->user=Customer::find($id);
-        $this->user->delete();
-    }
-
-    public function save()
-    {
-
-        if ($this->photo) {
-
-            $photoPath = $this->photo->storeAs("public/Imagenes", uniqid() . '-' . $this->photo->getClientOriginalName());
-            Customer::updateOrCreate(['id' => $this->id_Customer], [
-                'photo' => $photoPath,
-                'buisinessname' => $this->buisinessname,
-                'tradename' => $this->tradename,
-            ]);
+    public function mount()
+    { {
+            $this->setup(Customer::class, CustomerEvent::class, ["tradename", "businessname"], [
+                "tradename" => "",
+                "businessname" => "",
+            ], "businessname");
+            $this->items = $this->Model::all();
         }
-        $this->cleanfields();
-        $this->Offmodal();
     }
 }
