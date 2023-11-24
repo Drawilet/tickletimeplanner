0f76f44f405d2500657ce7dd95ba22d702d77753
Drawilet <drawiletdev@gmail.com>
2023-11-24 12:58:52 -0600

    Add setup script for Tickletime Planner

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# BEGIN: Setup Commands
+echo "Setting up Tickletime Planner..."
+
+# NPM INSTALL
+echo "Running 'npm install'..."
+npm install
+
+# COMPOSER INSTALL
+echo "Running 'composer install'..."
+composer install
+
+# COPY .ENV
+echo "Copying .env.example"
+cp .env.example .env
+
+# RUN MIGRATIONS
+echo "Running migrations"
+php artisan migrate:fresh --seed
+
+# END: Setup Commands
+echo "Tickletime Planner setup complete."
