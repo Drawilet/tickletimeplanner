4de3a4b411436eb45871d67d976ca5fd365dcf43
Drawilet <drawiletdev@gmail.com>
2023-11-27 10:09:01 -0600

    fix; change crud component design

--- resources/views/livewire/util/crud-component.blade.php
+++ resources/views/livewire/util/crud-component.blade.php
@@ -93,100 +93,101 @@
                 </div>
             </div>
 
-            @if ($items->isNotEmpty())
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            @foreach ($keys as $key)
-                                <th class="capitalize {{ $mainKey == $key ? '' : '' }}">{{ $key }}</th>
-                            @endforeach
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($showingItems as $item)
+            <div class="overflow-x-auto">
+                @if ($items->isNotEmpty())
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <td>{{ $item->id }}</td>
+                                <th></th>
                                 @foreach ($keys as $key)
-                                    <td class="  ">
-                                        @if (isset($specialInputs[$key]))
-                                            @switch($specialInputs[$key]["type"])
-                                                @case('file')
-                                                    @if (gettype($item[$key]) == 'string')
-                                                        <img src="{{ $item[$key] }}" alt=""
-                                                            class="w-10 h-10 rounded-full">
-                                                    @else
-                                                        <button class="btn btn-ghost"
-                                                            onclick="{{ $key }}Modal.showModal()">
-                                                            @component('components.icons.arrows-pointing-out')
-                                                            @endcomponent
-                                                        </button>
-                                                        <dialog id="{{ $key }}Modal"
-                                                            class="modal modal-bottom md:modal-middle">
-                                                            <div class="modal-box md:w-11/12 md:max-w-5xl">
-                                                                <h3 class="font-bold text-lg mb-2">{{ $Name }}
-                                                                    {{ $key }}</h3>
-                                                                <div class="flex flex-wrap gap-2">
-                                                                    @foreach ($item[$key] as $file)
-                                                                        <img src="{{ $file['url'] }}" alt=""
-                                                                            class="h-36  rounded border border-base-300">
-                                                                    @endforeach
-                                                                </div>
-
-                                                                <div class="modal-action">
-                                                                    <form method="dialog">
-                                                                        <button class="btn">Close</button>
-                                                                    </form>
-                                                                </div>
-                                                            </div>
-                                                            <form method="dialog" class="modal-backdrop">
-                                                                <button>close</button>
-                                                            </form>
-                                                        </dialog>
-                                                    @endif
-                                                @break
-
-                                                @case('textarea')
-                                                    <span>{{ \Illuminate\Support\Str::limit($item[$key], 50) }}</span>
-                                                @break
-
-                                                @default
-                                                    {{ $item[$key] }}
-                                                @break
-                                            @endswitch
-                                        @else
-                                            {{ $item[$key] }}
-                                        @endif
-                                    </td>
+                                    <th class="capitalize {{ $mainKey == $key ? '' : '' }}">{{ $key }}</th>
                                 @endforeach
-
-                                <td>
-                                    <button wire:click="Modal('save', true, '{{ $item->id }}')">
-                                        @component('components.icons.pencil-square')
-                                        @endcomponent
-                                    </button>
-                                    <button wire:click="Modal('delete', true, '{{ $item->id }}')">
-                                        @component('components.icons.trash')
-                                        @endcomponent
-                                    </button>
-                                </td>
-
+                                <th></th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @else
-                <div class="flex flex-col items-center justify-center mt-5">
-                    <h2 class="text-2xl opacity-90">No {{ $name }}s found</h2>
-                    <button wire:click="Modal('save', true)" class=" btn btn-primary py-2 px-4 mt-4">
-                        @component('components.icons.plus')
-                        @endcomponent
-                        Create {{ $name }}
-                    </button>
-                </div>
+                        </thead>
+                        <tbody>
+                            @foreach ($showingItems as $item)
+                                <tr class="hover">
+                                    <td>{{ $item->id }}</td>
+                                    @foreach ($keys as $key)
+                                        <td>
+                                            @if (isset($specialInputs[$key]))
+                                                @switch($specialInputs[$key]["type"])
+                                                    @case('file')
+                                                        @if (gettype($item[$key]) == 'string')
+                                                            <img src="{{ $item[$key] }}" alt=""
+                                                                class="w-10 h-10 rounded-full">
+                                                        @else
+                                                            <button class="btn btn-ghost"
+                                                                onclick="{{ $key }}Modal.showModal()">
+                                                                @component('components.icons.arrows-pointing-out')
+                                                                @endcomponent
+                                                            </button>
+                                                            <dialog id="{{ $key }}Modal"
+                                                                class="modal modal-bottom md:modal-middle">
+                                                                <div class="modal-box md:w-11/12 md:max-w-5xl">
+                                                                    <h3 class="font-bold text-lg mb-2">{{ $Name }}
+                                                                        {{ $key }}</h3>
+                                                                    <div class="flex flex-wrap gap-2">
+                                                                        @foreach ($item[$key] as $file)
+                                                                            <img src="{{ $file['url'] }}" alt=""
+                                                                                class="h-36  rounded border border-base-300">
+                                                                        @endforeach
+                                                                    </div>
 
-            @endif
+                                                                    <div class="modal-action">
+                                                                        <form method="dialog">
+                                                                            <button class="btn">Close</button>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                                <form method="dialog" class="modal-backdrop">
+                                                                    <button>close</button>
+                                                                </form>
+                                                            </dialog>
+                                                        @endif
+                                                    @break
+
+                                                    @case('textarea')
+                                                        <span>{{ \Illuminate\Support\Str::limit($item[$key], 50) }}</span>
+                                                    @break
+
+                                                    @default
+                                                        {{ $item[$key] }}
+                                                    @break
+                                                @endswitch
+                                            @else
+                                                {{ $item[$key] }}
+                                            @endif
+                                        </td>
+                                    @endforeach
+
+                                    <td>
+                                        <button wire:click="Modal('save', true, '{{ $item->id }}')">
+                                            @component('components.icons.pencil-square')
+                                            @endcomponent
+                                        </button>
+                                        <button wire:click="Modal('delete', true, '{{ $item->id }}')">
+                                            @component('components.icons.trash')
+                                            @endcomponent
+                                        </button>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <div class="flex flex-col items-center justify-center mt-5">
+                        <h2 class="text-2xl opacity-90">No {{ $name }}s found</h2>
+                        <button wire:click="Modal('save', true)" class=" btn btn-primary py-2 px-4 mt-4">
+                            @component('components.icons.plus')
+                            @endcomponent
+                            Create {{ $name }}
+                        </button>
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 
