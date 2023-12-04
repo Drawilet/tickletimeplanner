aacf3414fb43b55d07ed3e55a98386c613fb77bd
Sramos2077 <2110303@utmatamoros.edu.mx>
2023-12-04 13:51:40 -0600

    feat: adding upload photos

--- resources/views/livewire/settings/show.blade.php
+++ resources/views/livewire/settings/show.blade.php
@@ -13,31 +13,43 @@ x-data="{
     <div class="w-full flex justify-center">
         <div class="bg-base rounded-lg w-1/2 shadow-xl pb-8">
             <div class="w-full h-[250px]">
-                <img src="" alt="" class="mx-auto overflow-hidden rounded-full max-h-32">
-                <input wire:model="" type="file" id="" class="hidden">
+                <div class="mx-auto w-full h-[250px] bg-base-300 rounded-md overflow-hidden relative">
+                    @if($image_background)
+                    <img src="{{ $image_background->temporaryUrl() }}" class="mx-auto overflow-hidden rounded-md w-1/2">
+                    @else
+                    <label for="image-background"
+                    class="w-full h-[250px] bottom-0  bg-base-100 absolute flex items-center justify-center opacity-0 hover:opacity-75 transition duration-150 ease-in-out">
+                    <img src="/iconphoto.svg" alt="" class="mx-auto overflow-hidden rounded-md w-1/2">
+                    <input wire:model="image_background" type="file" id="image-background" class="hidden">
+                    @endif
+                </div>
             </div>
             <div class="flex flex-col items-center -mt-20">
                 <div class="mx-auto h-32 w-32 bg-base-300 rounded-full overflow-hidden relative">
-                    <img src="" alt="" class="mx-auto overflow-hidden rounded-full max-h-32">
-                        <label for="data.photo"
+                    @if ($image_profile)
+                        <img src="{{ $image_profile->temporaryUrl() }}" alt="" class="mx-auto overflow-hidden rounded-full max-h-32">
+                    @else
+                        <label for="image-profile"
                         class="w-32 h-12 bottom-0  bg-base-100 absolute flex items-center justify-center opacity-0 hover:opacity-75 transition duration-150 ease-in-out">
                         <img src="/iconphoto.svg">
-                        <input wire:model="data.photo" type="file" id="data.photo" class="hidden">
+                        <input wire:model="image_profile" type="file" id="image-profile" class="hidden">
+                    @endif
                 </div>
                 <div class="flex items-center space-x-2 mt-2">
-                    <p class="text-2xl">Your name</p>
+                    <p class="text-2xl">name: {{ $name }}</p>
+                    <p class="text-2xl">lastname: {{ $lastname }}</p>
                 </div>
-                <p class="text-gray-700">Your bio</p>
-                <p class="text-sm text-gray-500">where are you?</p>
+                <p class="text-gray-700">Your bio: {{ $bio }}</p>
+                <p class="text-sm text-gray-500">where are you?: {{ $location }}</p>
                 <x-form-control>
-                    <x-labell for="">Name</x-labell>
-                    <x-input id="" name="" wire:model=""/>
-                    <x-labell for="">Last name</x-labell>
-                    <x-input id="" name="" wire:model=""/>
-                    <x-labell for="">Bio</x-labell>
-                    <x-input id="" name="" wire:model=""/>
-                    <x-labell for="">Location</x-labell>
-                    <x-input id="" name="" wire:model=""/>
+                    <x-labell for="name">Name</x-labell>
+                    <x-input id="name" name="name" wire:model="name"/>
+                    <x-labell for="lastname">Last name</x-labell>
+                    <x-input id="lastname" name="lastname" wire:model="lastname"/>
+                    <x-labell for="bio">Bio</x-labell>
+                    <x-input id="bio" name="bio" wire:model="bio"/>
+                    <x-labell for="location">Location</x-labell>
+                    <x-input id="location" name="location" wire:model="location"/>
                 </x-form-control>
                     <template x-for="link in links">
                         <div class="flex space-x-2 items-center">
@@ -50,9 +62,9 @@ x-data="{
                             <div x-show="link.url.includes('tiktok')">
                                 <x-icons.tiktok />
                             </div>
-                            <div x-show="link.url.includes('twitter')">
+                            {{-- <div x-show="link.url.includes('twitter')">
                                 <x-icons.twitter />
-                            </div>
+                            </div> --}}
                             <div x-show="link.url.includes('linkedin')">
                                 <x-icons.linkedin />
                             </div>
@@ -64,7 +76,7 @@ x-data="{
                         </div>
                     </template>
             </div>
-                <button class="btn btn-success">Save</button>
+                <button class="btn btn-success" wire:click="getValues">Save</button>
                 <button class="btn btn-active btn-ghost">Cancel</button>
             </div>
         </div>
