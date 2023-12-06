dcaa92915e8af6010b4049c47efd245822069eea
Alonzo997 <alonzojesuseduardo@gmail.com>
2023-12-06 14:43:40 -0600

    Update SVG icons with dynamic class

--- resources/views/components/icons/chevron-right.blade.php
+++ resources/views/components/icons/chevron-right.blade.php
@@ -1,4 +1,5 @@
+@props(['class' => 'w-6 h-6'])
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-    class="w-6 h-6">
+    class="{{ $class }}">
     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
 </svg>
