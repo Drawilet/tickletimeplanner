9e2e11b69437cf1d21a43370c6bc829a76bf4b3c
Drawilet <drawiletdev@gmail.com>
2023-12-11 13:27:57 -0600

    Update description column type to text in tenants table

--- database/migrations/0000_00_00_000000_create_tenants_table.php
+++ database/migrations/0000_00_00_000000_create_tenants_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('phone');
             $table->string('email');
 
