fe7fea0eab63b4a136d0c00e58f130ee097ae53e
Alonzo997 <alonzojesuseduardo@gmail.com>
2023-12-12 13:28:50 -0600

    Update language translations in show-component and day.blade.php

--- resources/views/vendor/livewire-calendar/day.blade.php
+++ resources/views/vendor/livewire-calendar/day.blade.php
@@ -16,7 +16,7 @@
                 </p>
                 <p class="text-xs text-gray-600 ml-4">
                     @if ($events->isNotEmpty())
-                        {{ $events->count() }} {{ Str::plural('event', $events->count()) }}
+                        {{ $events->count() }} {{ Str::plural(__('calendar-lang.event'), $events->count()) }}
                     @endif
                 </p>
             </div>
