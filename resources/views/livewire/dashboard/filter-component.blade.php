8eb36737b59065d3d67fd850d96295f828e9e209
Drawilet <drawiletdev@gmail.com>
2023-12-13 14:04:27 -0600

    feat: add space filter color

--- resources/views/livewire/dashboard/filter-component.blade.php
+++ resources/views/livewire/dashboard/filter-component.blade.php
@@ -1,4 +1,11 @@
 <div>
+    <style>
+        .checkbox:checked {
+            background: currentColor;
+        }
+
+
+    </style>
     <div class="dropdown md:dropdown-end">
         <div tabindex="0" role="button" class="btn m-1 px-10">Spaces</div>
         <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
@@ -13,6 +20,7 @@
                 <li>
                     <label for="{{ $space->id }}">
                         <input id="{{ $space->id }}" type="checkbox" class="checkbox"
+                            @if (in_array($space->id, $filters['spaces'])) style="background: {{ $space->color }}" @endif
                             wire:click="toggleSpace('{{ $space->id }}')" @checked(in_array($space->id, $filters['spaces'])) />
                         {{ $space['name'] }}
                     </label>
