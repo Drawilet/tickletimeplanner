1e66a1485b88023bd9c6375653b86cad371f37f1
Alonzo997 <alonzojesuseduardo@gmail.com>
2023-12-14 13:29:07 -0600

    Add language files for English and Spanish, and update home component view

---
+++ lang/es/home-lang.php
@@ -0,0 +1,10 @@
+<?php
+return [
+
+    'Instant Reservations' => 'Reservaciones instantáneas',
+    'p1' => '
+Facilita a los usuarios reservar rápidamente y obtener un espacio sencillo para eventos, ya sea para reuniones corporativas, fiestas privadas u otras ocasiones.',
+    'Ourservices' => 'Nuestros servicios',
+    'p2' => 'Ofrecemos una solución integral para la gestión de reservas, citas y eventos, con una interfaz amigable y funciones que mejoran la eficiencia y la satisfacción del usuario en el proceso de planificación.',
+    'Booknow' => 'Reservar ahora',
+];
