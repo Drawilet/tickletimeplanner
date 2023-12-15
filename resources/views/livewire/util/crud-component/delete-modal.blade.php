1f2d3af509b69df07c6d0181a2481eadce06a9e1
Drawilet <drawiletdev@gmail.com>
2023-12-15 10:30:44 -0600

    Update button and modal styles

--- resources/views/livewire/util/crud-component/delete-modal.blade.php
+++ resources/views/livewire/util/crud-component/delete-modal.blade.php
@@ -7,9 +7,9 @@
     </x-slot>
     <x-slot name="footer">
         <button wire:click="Modal('delete', false)" type="button"
-            class="btn btn-neutral w-28">{{ __('show-lang.cancel') }}</button>
+            class="btn w-28">{{ __('show-lang.cancel') }}</button>
         <button wire:click.prevent="delete()" type="button"
-            class="btn btn-warning w-28 mr-2">{{ __('show-lang.delete') }}</button>
+            class="btn btn-error w-28 ml-2">{{ __('show-lang.delete') }}</button>
 
     </x-slot>
 </x-dialog-modal>
