623b73187932c778ff9141c9e95fafbbaed6b208
Drawilet <Drawilet@users.noreply.github.com>
2024-01-24 18:03:03 +0000

    Prettified Code!

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
 });
