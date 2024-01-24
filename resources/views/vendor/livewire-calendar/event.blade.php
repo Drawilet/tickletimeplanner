0ed2fb9f7f077c1e92bed8095b56553f91329661
Drawilet <drawiletdev@gmail.com>
2024-01-24 13:06:21 -0600

    Add isDraft property to event data

--- resources/views/vendor/livewire-calendar/event.blade.php
+++ resources/views/vendor/livewire-calendar/event.blade.php
@@ -1,6 +1,6 @@
 <div @if ($eventClickEnabled) wire:click.stop="onEventClick('{{ $event['id'] }}')" @endif
-    class="bg-neutral rounded-lg border py-2 px-3 shadow-md cursor-pointer text-black"
-    style="background-color: {{ $event['color'] }}">
+    class="bg-neutral rounded-lg border py-2 px-3 shadow-md cursor-pointer text-black {{ $event['isDraft'] ? 'opacity-50' : '' }}"
+    style="background-color: {{ $event['isDraft'] ? '#EFEFEF' : $event['color'] }}">
 
     <p class="text-sm font-medium">
         {{ $event['title'] }} <sup>({{ $event['location'] }})</sup>
