12e576c0dea47c1bd6d05e7dfa3e8335104aaeb5
Drawilet <drawiletdev@gmail.com>
2024-01-26 12:29:21 -0600

    Add profile photo feature to navbar component

--- resources/views/components/navbar-component.blade.php
+++ resources/views/components/navbar-component.blade.php
@@ -15,22 +15,14 @@
 
         <span class="mr-2"></span>
 
-        <x-dropdown align="right" width="48">
-            <x-slot name="trigger">
-                <span class="inline-flex rounded-md">
-                    <button type="button"
-                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md bg-base-300">
-                        {{ Auth::user()->name }}
 
-                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                        </svg>
-                    </button>
-                </span>
-            </x-slot>
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn m-1">
+                {{ Auth::user()->name }}
 
-            <x-slot name="content">
+                <img src="/storage/{{ Auth::user()->profile_photo_path }}" alt="" class="h-7">
+            </div>
+            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
                 <!-- Account Management -->
                 <div class="block px-4 py-2 text-xs">
                     {{ __('navbar-lang.ManageAccount') }}
@@ -49,7 +41,7 @@
                         {{ __('navbar-lang.LogOut') }}
                     </x-dropdown-link>
                 </form>
-            </x-slot>
-        </x-dropdown>
+            </ul>
+        </div>
     </div>
 </div>
