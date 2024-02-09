2a1f696e3981790386e7fb509cc8d4becbbb883e
Drawilet <drawiletdev@gmail.com>
2024-02-09 10:37:07 -0600

    Update dependencies and refactor code

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -33,18 +33,3 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
-
-import Echo from "laravel-echo";
-import Pusher from "pusher-js";
-
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: "tickletimeplanner",
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
-    cluster: "eu",
-});
