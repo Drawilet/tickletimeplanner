07117b7f7fe3bb1e5963eca38c46b32dc3e9a2e2
Drawilet <drawiletdev@gmail.com>
2024-03-06 13:05:09 -0600

    Fix form tag in crud-component.blade.php

--- resources/views/livewire/util/crud-component.blade.php
+++ resources/views/livewire/util/crud-component.blade.php
@@ -6,7 +6,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-base-100 overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
             <div class="mockup-browser border border-base-300">
-                <div class="mockup-browser-toolbar flex items-center ">
+                <form autocomplete="off" class="mockup-browser-toolbar flex items-center ">
                     <div class="hidden lg:flex dots">
                         <div class="dot animate-bounce-1" id="Myelement-1" wire:target="">&#9679;</div>
                         <div class="dot animate-bounce-2" id="Myelement-2" wire:target="">&#9679;</div>
@@ -15,14 +15,14 @@
                     <input id="crud-search" class="input py-5" wire:model.debounce.500ms="filter.search"
                         wire:keyup.debounce.500ms="filterUpdated" type="text"
                         placeholder="{{ __($name . '-lang.' . 'search') }}">
-                    <button wire:click="Modal('save', true)"
+                    <button wire:click="Modal('save', true)" type="button"
                         class=" btn btn-ghost bg-base-100 hover:bg-base-300 hover:text-blue-50 py-2 px-4 ml-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                     </button>
-                </div>
+                </form>
             </div>
             @include('livewire.util.crud-component.table')
         </div>
