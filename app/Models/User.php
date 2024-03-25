95b587a6bcd6feda6d202364ae632ec857c11ce5
Drawilet <drawiletdev@gmail.com>
2024-03-25 09:26:04 -0500

    Add email verification feature to User model and enable it in Fortify configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
