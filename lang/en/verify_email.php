3c6c9dc530c631220fb130f84c8ca06bf589a553
Alonzo997 <alonzojesuseduardo@gmail.com>
2024-03-25 10:20:48 -0500

    Agrega archivos de idioma y actualiza la vista de verificación de correo electrónico

---
+++ lang/en/verify_email.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'verify_email' => 'Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another',
+    'resend-email'=>'Resend Verification Email',
+    'edit_profile'=>'Edit Profile',
+    'logout'=>'Logout',
+    'new_verficication_link'=>'A new verification link has been sent to the email address you provided during registration.',
+   
+
+
+];
