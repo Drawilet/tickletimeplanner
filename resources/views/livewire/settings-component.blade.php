f6d207c3fbee51deea1517838f8c416eeca128a7
Drawilet <drawiletdev@gmail.com>
2024-04-10 11:50:18 -0500

    remove unused div

--- resources/views/livewire/settings-component.blade.php
+++ resources/views/livewire/settings-component.blade.php
@@ -1,9 +1,7 @@
-<div>
-    @role('app.admin')
-        @livewire('settings.app-settings-component')
-    @endrole
+@role('app.admin')
+    @livewire('settings.app-settings-component')
+@endrole
 
-    @role('tenant.admin')
-        @livewire('settings.tenant-settings-component')
-    @endrole
-</div>
+@role('tenant.admin')
+    @livewire('settings.tenant-settings-component')
+@endrole
